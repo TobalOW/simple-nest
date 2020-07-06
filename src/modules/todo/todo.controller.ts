cac62e91ed82ecedf27c540a275a2f80da5242f2
TobalOW <cristobal.miranda.u@gmail.com>
2020-07-05 21:04:29 -0400

    feat(core): redirect with 301 in todo controller

--- src/modules/todo/todo.controller.ts
+++ src/modules/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Get } from '@nestjs/common';
+import { Controller, Post, Get, Redirect } from '@nestjs/common';
 
 @Controller('todo')
 export class TodoController {
@@ -10,5 +10,12 @@ export class TodoController {
   @Get()
   findAll(): string {
     return 'This action returns all todos';
+  }
+  
+// Simple redirect
+  @Get(':id')
+  @Redirect('https://nestjs.com', 301)
+  getDocs(): boolean {
+    return true;
   }
 }
